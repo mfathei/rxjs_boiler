6dea782f91223864ce58d93b3ec366f1b09a9c08
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-03-04 14:45:29 +0200

    observable from events

--- src/app.js
+++ src/app.js
@@ -1,4 +1,49 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('RxJS boiler running');
+const btn = $('#btn');
+const btnStream$ = Rx.Observable.fromEvent(btn, 'click');
+
+btnStream$.subscribe(
+    function (event) {
+        console.log(event.target.innerHTML);
+    },
+    function (err) {
+        console.error(err);
+    },
+    function () {
+        console.log('completed');
+    }
+);
+
+const input = $('#input');
+const inputStream$ = Rx.Observable.fromEvent(input, 'keyup');
+
+inputStream$.subscribe(
+    function (event) {
+        console.log(event.target.value);
+    },
+    function (err) {
+        console.error(err);
+    },
+    function () {
+        console.log('completed');
+    }
+);
+
+
+const output = $('#output');
+const outputStream$ = Rx.Observable.fromEvent(document, 'mousemove');
+
+outputStream$.subscribe(
+    function (event) {
+        console.log(event.target.value);
+        output.html('<h1>X: ' + event.clientX + ' Y: ' + event.clientY + '</h1>');
+    },
+    function (err) {
+        console.error(err);
+    },
+    function () {
+        console.log('completed');
+    }
+);
